a59a0e7b79b7c80c73f6076477ff19d16c5de506
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-08-29 17:20:15 +0000

    filtered logger

--- filtered_logger.py
+++ filtered_logger.py
@@ -1,6 +1,18 @@
+#!/usr/bin/env python3
+
 import re
 
-def filter_datum(fields, redaction, message, separator):
-    pattern = f"({'|'.join(fields)})=([^ {separator}]*)"
-    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
 
+def filter_datum(fields, redaction, message, separator):
+    """
+    filtered datum
+    Args:
+        fields(dict): fileds to be inputed
+        redaction(str): the hashing character
+        message(str): the logged texts
+        separator(str): separates filed name and value
+    Return:
+        returns string
+    """
+    pattern = f"({'|'.join(fields)})=([^ {separator}]*)"
+    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)  # noqa
