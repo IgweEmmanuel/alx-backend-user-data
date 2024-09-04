c43df352d699e24b4f9513df7da9345877c43ae4
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-09-04 17:19:11 +0000

    update auth

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+This is the authentication file to authenticate
+users
+"""
+from flask import request
+from typing import List
+
+
+class Auth:
+    """ The authentication class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        The authentication public method
+        Args:
+            path(str): this is the url path
+            excluded_paths(List): this is the excluded path
+        Return:
+            - gives bool
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        The authorization header
+        Args:
+            request(None): the request
+        Return:
+            - returns string
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        The current user
+        Args:
+            request(None): the request
+        Return:
+            - returns User
+        """
+        return None
