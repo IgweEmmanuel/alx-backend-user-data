305cee7e96fa7968202afed406e9742cbe06b2a1
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-09-06 21:29:53 +0000

    update basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -44,3 +44,24 @@ class BasicAuth(Auth):
             return base64_decode.decode('utf-8')
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):  # noqa
+        """
+        decoded authorization header
+        Args:
+            decoded_base64_authorization_header(str): parameter
+        Return:
+            - returns decoded authorization
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+
+        email, password = decoded_base64_authorization_header.split(':',1)
+        return (email, password)
+
