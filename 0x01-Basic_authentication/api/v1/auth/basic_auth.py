ab9558cac1de484f7232441e59cc074ec1cc6204
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-09-06 16:42:33 +0000

    update basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -22,4 +22,4 @@ class BasicAuth(Auth):
 
         if not authorization_header.startswith('Basic '):
             return None
-        return authorization_header.split('Basic')[1]
+        return authorization_header.split('Basic ')[1]
