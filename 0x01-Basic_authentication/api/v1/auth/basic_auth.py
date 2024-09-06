98264d60cb0ddda82103e0b9b74d603023a22717
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-09-06 15:54:44 +0000

    update basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,21 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """BasicAuth class"""
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:  # noqa
+        """
+        Extracts base64
+        Args:
+            authorization_header(str): authorize
+        Returns:
+            - Returns base64 encoding
+        """
+        if authorization_header is None:
+            return None
+
+        if not isinstance(str, authorization_header):
+            return None
+
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[5:]
