747351f06a3cd4dcf4d0f82aa7edb6b7bed2bc6e
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-09-07 03:47:04 +0000

    update basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,10 @@
 """Basic Authorization class"""
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
+
+User = TypeVar('User')
 
 
 class BasicAuth(Auth):
@@ -65,3 +69,27 @@ class BasicAuth(Auth):
         # the number 1 stands for first occurence of ':' in the str
         email, password = decoded_base64_authorization_header.split(':', 1)  # noqa
         return (email, password)
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):  # noqa
+        """
+        user credentials
+        Args:
+            user_email(str): user email
+            user_pwd(str): user password
+        Return:
+            - returns user email and password
+        """
+        user = User.search(user_email)
+
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        if user is None:
+            return None
+
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
